aa25d75cef9a70f00e5092795716df4ef243e34c
ngohipha <ngohipha@gmail.com>
2023-05-17 17:05:29 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 
 const io = new Server(server, {
-  cors: "*",
-  methods: "*",
+  cors: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PATCH', "DELETE"]
 });
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes')
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/users', userRoutes)
+
 
 server.listen(8080, () => {
   console.log("server running at port", 8080);
 });
+
+app.set('socketio',io)
