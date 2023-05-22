d3a6a07d0d241a9585a02c60888cdcf51eaf3a1f
ngohipha <ngohipha@gmail.com>
2023-05-22 11:16:59 +0700

    update order

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 const http = require("http");
 require("./connection");
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
@@ -14,6 +16,7 @@ const User = require('./models/User');
 const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const imageRoutes = require ('./routes/imageRoutes')
+const orderRoutes = require ('./routes/orderRoutes')
 
 
 app.use(cors());
@@ -22,6 +25,24 @@ app.use(express.json());
 app.use('/users', userRoutes)
 app.use('/products', productRoutes)
 app.use('/images' , imageRoutes)
+app.use('/orders' , orderRoutes)
+
+
+app.post('/create-payment', async(req, res)=> {
+  const {amount} = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+    });
+    res.status(200).json(paymentIntent)
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json(e.message);
+   }
+})
+
 server.listen(8080, () => {
   console.log("server running at port", 8080);
 });
